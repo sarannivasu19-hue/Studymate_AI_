d34794d124f2a96b12ef3317486a22f29ccd3634
Saran Nivasu S <sarannivasu19@gmail.com>
2026-07-06 13:38:02 +0530

    Fix language API

--- frontend/src/services/languageApi.js
+++ frontend/src/services/languageApi.js
@@ -1,12 +1,16 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/language";
+const API_URL =
+  import.meta.env.VITE_API_URL + "/api/language";
 
 export async function askAI(question, language) {
-  const response = await axios.post(`${API_URL}/ask`, {
-    question,
-    language,
-  });
+  const response = await axios.post(
+    `${API_URL}/ask`,
+    {
+      question,
+      language,
+    }
+  );
 
   return response.data;
 }
